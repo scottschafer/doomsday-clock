3585beb8ff2e1626bc54dc4c3d228ae494040473
Scott Schafer <scott.schafer@gmail.com>
2015-11-18 09:29:45 -0800

    Revert "touch"
    This reverts commit 4475fef35391f6644a6a23eefa7d395cf7bf473b.

--- public/config.js
+++ public/config.js
@@ -4,8 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'doomsday-clock';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',
-    'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
